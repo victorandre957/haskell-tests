07424deae223cbe563e2216785404d363f59f65f
Victor André <victorandre957@gmail.com>
2023-05-15 22:21:16 -0300

    Trabalho 2 Q1

--- testes/Testes.hs
+++ testes/Testes.hs
@@ -1,94 +1,283 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use &&" #-}
 module Main where
 
-import Trabalho_1
+import Trabalho_2
+import Dados
 
-import Data.List (sort)
+import Data.Maybe (isNothing)
 import Test.Hspec
 
 
-caso11 = maior4 0 0 0 0 == 0
-caso12 = maior4 0 1 0 0 == 1
-caso13 = maior4 3 3 3 2 == 3
-caso14 = maior4 3 2 1 4 == 4
+--------------------- DECLARAÇÕES ---------------------
+med1 :: Medicamento
+med1 = "Adera"
 
-testeExercicio1 = and [caso11, caso12, caso13, caso14]
+med2 :: Medicamento
+med2 = "Alprazolam"
 
-caso21 = converterNotaParaMencao 10 == "SS"
-caso22 = converterNotaParaMencao 9.5 == "SS"
-caso23 = converterNotaParaMencao 8.9 == "MS"
-caso24 = converterNotaParaMencao 7 == "MS"
-caso25 = converterNotaParaMencao 5 == "MM"
-caso26 = converterNotaParaMencao 3.9 == "MI"
-caso27 = converterNotaParaMencao 2.5 == "II"
-caso28 = converterNotaParaMencao 0.1 == "II"
-caso29 = converterNotaParaMencao 0 == "SR"
+med3 :: Medicamento
+med3 = "Donepezila"
 
-testeExercicio2 = and [caso21, caso22, caso23, caso24, caso25, caso26, caso27, caso28, caso29]
+med4 :: Medicamento
+med4 = "Lactulona"
 
-caso31 = isDecrescente []
-caso32 = isDecrescente [2]
-caso33 = isDecrescente [3,2,1]
-caso34 = not (isDecrescente [3,2,1,4])
-caso35 = not (isDecrescente [3,2,1,1])
-caso36 = not (isDecrescente [1,1,1,1])
-caso37 = isDecrescente [35,7,4,0,-3]
-testeExercicio3 = and [caso31,caso32,caso33,caso34,caso35,caso36,caso37]
+med5 :: Medicamento
+med5 = "Mirtazapina"
 
-caso41 = sort (histograma []) == sort []
-caso42 = sort (histograma ["Batata"]) == [("Batata", 1)]
-caso43 = sort (histograma ["Tomate", "Pepino", "Cenoura", "Tomate"]) == sort [("Tomate", 2),("Pepino", 1),("Cenoura", 1)]
-caso44 = sort (histograma ["Mamão", "Mamão", "Mamão", "Mamão"]) == sort [("Mamão", 4)]
-caso45 = sort (histograma ["Chocolate", "Bolo", "Chocolate", "Bolo", "Bolo", "Chocolate"]) == sort [("Chocolate", 3), ("Bolo", 3)]
-testeExercicio4 = and [caso41, caso42, caso43, caso44, caso45]
+med6 :: Medicamento
+med6 = "Pantoprazol"
 
-caso51 = null $ myZipWith (+) [1,2] []
-caso52 = null $ myZipWith (+) [] [1,2]
-caso53 = myZipWith (+) [1,2] [1,2] == [2,4]
-caso54 = myZipWith (+) [1,2,3] [1,2] == [2,4]
-caso55 = myZipWith (+) [1,2] [1,2,3] == [2,4]
-caso56 = myZipWith (+) [7,5,3,1] [1,3,5,7] == [8,8,8,8]
-testeExercicio5 = and [caso51, caso52, caso53, caso54, caso55, caso56]
+med7 :: Medicamento
+med7 = "Patz"
 
-caso61 = null $ aprovadosOrdemDeMedia []
-caso62 = aprovadosOrdemDeMedia [("Alberto",6,8)] == [("Alberto", (6+8)/2.0)]
-caso63 = null (aprovadosOrdemDeMedia [("Alberto",0,9.9)])
-caso64 = aprovadosOrdemDeMedia [("Alberto",5,5)] == [("Alberto", (5+5)/2.0)]
-caso65 = aprovadosOrdemDeMedia [("Alberto",0,10)] == [("Alberto", (0+10)/2.0)]
+med8 :: Medicamento
+med8 = "Quetiapina"
 
-caso66 = aprovadosOrdemDeMedia [("Primeiro",9,10),("Segundo",8,9),("Penultimo",6,7),("Ultimo",5,5),("Reprovado",4,5)]
-        == [("Ultimo",(5+5)/2),("Penultimo",(6+7)/2),("Segundo",(8+9)/2),("Primeiro",(9+10)/2)]
+med9 :: Medicamento
+med9 = "Xarelto"
 
-testeExercicio6 = and [caso61, caso62, caso63, caso64, caso65, caso66]
+estoque1 :: EstoqueMedicamentos
+estoque1 = [(med4, 10), (med6, 5), (med7, 0)]
 
+estoque2 :: EstoqueMedicamentos
+estoque2 = [(med4, 10), (med6, 5), (med7, 10)]
 
-caso71a = null (somaMatricial [] [])
-caso72a = somaMatricial [[1,2,3]] [[3,2,1]] == [[4,4,4]]
-caso73a = somaMatricial [[1,2,3],[4,5,6]] [[-1,-2,-3],[-4,-5,-6]] == [[0,0,0],[0,0,0]]
-caso74a = somaMatricial [[3]] [[4]] == [[7]]
-caso75a = somaMatricial [[1],[2],[3]] [[4],[5],[6]] == [[5],[7],[9]]
+estoque3 :: EstoqueMedicamentos
+estoque3 = [(med4, 10), (med6, 50), (med7, 10), (med8, 20)]
 
-caso71b = null (matrizTransposta [])
-caso72b = matrizTransposta [[1,2,3]] == [[1],[2],[3]]
-caso73b = matrizTransposta [[1],[2],[3]] == [[1,2,3]]
-caso74b = matrizTransposta [[1,2,3],[4,5,6],[7,8,9]] == [[1,4,7],[2,5,8],[3,6,9]] 
+receituario1 :: Receituario
+receituario1 = [(med4, [8, 17]), (med6, [6]), (med7, [22])]
 
-caso71c = null (multiplicacaoMatricial [] [])
-caso72c = multiplicacaoMatricial [[1,2],[3,4]] [[5,6],[7,8]] == [[19,22],[43,50]]
-caso73c = multiplicacaoMatricial [[1]] [[2]] == [[2]]
-caso74c = multiplicacaoMatricial [[1,2,3]] [[4],[5],[6]] == [[32]]
-caso75c = multiplicacaoMatricial [[1],[2],[3]] [[4,5,6]] == [[4,5,6],[8,10,12],[12,15,18]]
+receituario2 :: Receituario
+receituario2 = [(med4, [8, 17]), (med6, [6]), (med7, [22]), (med8, [8, 22, 23])]
 
-testeExercicio7a = and [caso71a, caso72a, caso73a, caso74a, caso75a]
-testeExercicio7b = and [caso71b, caso72b, caso73b, caso74b]
-testeExercicio7c = and [caso71c, caso72c, caso73c, caso74c, caso75c]
+receituarioInvalido1 :: Receituario
+-- Invalido: horário não está ordenado de forma crescente
+receituarioInvalido1 = [(med4, [22, 10])]
 
+receituarioInvalido2 :: Receituario
+-- Invalido: medicamentos não estão ordenbados de forma crescente
+receituarioInvalido2 = [(med4, [8, 17]), (med6, [6]), (med7, [22]), (med2, [8, 22, 23])]
 
+receituarioInvalido3 :: Receituario
+-- Invalido: medicamentos não estão ordenbados de forma crescente
+receituarioInvalido3 = [(med4, [8, 17]), (med3, [6]), (med7, [22])]
+
+receituarioInvalido4 :: Receituario
+-- Invalido: horário não estão ordenbados de forma crescente
+receituarioInvalido4 = [(med4, [8, 17]), (med6, [6]), (med7, [22]), (med8, [8, 23, 22])]
+
+plano1 :: PlanoMedicamento
+plano1 = [(6, [med6]), (8, [med4]), (17, [med4]), (22, [med7])]
+
+plano2 :: PlanoMedicamento
+plano2 = [(6, [med6]), (8, [med4, med8]), (17, [med4]), (22, [med7, med8]), (23, [med8])] :: [(Int, [String])]
+
+planoInvalido1 :: PlanoMedicamento
+-- Invalido: horário não está ordenado de forma crescente
+planoInvalido1 = [(8, [med4]), (6, [med6]), (17, [med4]), (22, [med7])]
+
+planoInvalido2 :: PlanoMedicamento
+-- Invalido: medicamentos não estão ordenbados de forma crescente
+planoInvalido2 = [(6, [med6]), (8, [med8, med4]), (17, [med4]), (22, [med7, med8]), (23, [med8])] :: [(Int, [String])]
+
+planoInvalido3 :: PlanoMedicamento
+-- Invalido: medicamentos não estão ordenbados de forma crescente
+planoInvalido3 = [(6, [med6]), (8, [med4, med8]), (17, [med4]), (22, [med8, med7]), (23, [med8])] :: [(Int, [String])]
+
+planoInvalido4 :: PlanoMedicamento
+-- Invalido: horário não está ordenado de forma crescente
+planoInvalido4 = [(6, [med6]), (8, [med4, med8]), (17, [med4]), (23, [med8]), (22, [med7, med8])] :: [(Int, [String])]
+
+plantao1 :: Plantao
+plantao1 =
+  [ (6, [Medicar med6]),
+    (8, [Medicar med4]),
+    (17, [Medicar med4]),
+    (22, [Medicar med7])
+  ]
+
+plantao2 :: Plantao
+plantao2 =
+  [ (6, [Medicar med6]),
+    (8, [Medicar med4]),
+    (17, [Medicar med4, Comprar med7 30]),
+    (22, [Medicar med7])
+  ]
+
+plantao3 :: Plantao
+plantao3 =
+  [ (6, [Medicar med6, Medicar med9]),
+    (8, [Medicar med2, Medicar med4]),
+    (17, [Medicar med4, Comprar med7 30]),
+    (22, [Medicar med7])
+  ]
+
+plantaoInvalido1 :: Plantao
+plantaoInvalido1 =
+  [ (6, [Medicar med6, Medicar med9]),
+    (8, [Medicar med2, Medicar med4]),
+    (22, [Medicar med7]),
+    -- Invalido: não está em horario crescente
+    (17, [Medicar med4, Comprar med7 30])
+  ]
+
+plantaoInvalido2 :: Plantao
+plantaoInvalido2 =
+  [ (6, [Medicar med6, Medicar med9]),
+    (8, [Medicar med2, Medicar med4]),
+    -- Invalido: comprar o mesmo medicamento no horário
+    (17, [Medicar med4, Comprar med4 30]),
+    (22, [Medicar med7])
+  ]
+
+plantaoInvalido3 :: Plantao
+plantaoInvalido3 =
+  [ (6, [Medicar med6, Medicar med9]),
+    -- Invalido: ordem dos medicamentos não está ordenada
+    (8, [Medicar med4, Medicar med2]),
+    (17, [Medicar med4, Comprar med7 30]),
+    (22, [Medicar med7])
+  ]
+
+plantaoInvalido4 :: Plantao
+plantaoInvalido4 =
+  [ (6, [Medicar med6]),
+    -- Invalido: comprar o mesmo medicamento no horário
+    (8, [Comprar med4 20, Medicar med4, Medicar med8]),
+    (17, [Medicar med4]),
+    (22, [Medicar med7, Medicar med8]),
+    (23, [Medicar med8])
+  ]
+
+--------------------- EXERCÍCIO 1 ---------------------
+caso1_1 = comprarMedicamento med7 30 estoque1 == [(med4, 10), (med6, 5), (med7, 30)]
+
+caso1_2 = comprarMedicamento med1 20 estoque1 == [(med1, 20), (med4, 10), (med6, 5), (med7, 0)]
+
+caso1_3 = comprarMedicamento med6 2 estoque1 == [(med4, 10), (med6, 7), (med7, 0)]
+
+caso1_4 = comprarMedicamento med9 20 [] == [(med9, 20)]
+
+testeExercicio1 = and [caso1_1, caso1_2, caso1_3, caso1_4]
+
+--------------------- EXERCÍCIO 2 ---------------------
+caso2_1 = tomarMedicamento med4 estoque1 == Just [(med4, 9), (med6, 5), (med7, 0)]
+
+caso2_2 = isNothing (tomarMedicamento med8 estoque1)
+
+testeExercicio2 = and [caso2_1, caso2_2]
+
+--------------------- EXERCÍCIO 3 ---------------------
+caso3_1 = consultarMedicamento med6 estoque1 == 5
+
+caso3_2 = consultarMedicamento "Aas" estoque1 == 0
+
+testeExercicio3 = and [caso3_1, caso3_2]
+
+--------------------- EXERCÍCIO 4 ---------------------
+caso4_1 = demandaMedicamentos receituario1 == [(med4, 2), (med6, 1), (med7, 1)]
+
+caso4_2 = demandaMedicamentos receituario2 == [(med4, 2), (med6, 1), (med7, 1), (med8, 3)]
+
+testeExercicio4 = and [caso4_1, caso4_2]
+
+--------------------- EXERCÍCIO 5a ---------------------
+caso5_1a = receituarioValido receituario1
+
+caso5_2a = receituarioValido receituario2
+
+caso5_3a = not $ receituarioValido receituarioInvalido1
+
+caso5_4a = not $ receituarioValido receituarioInvalido2
+
+caso5_5a = not $ receituarioValido receituarioInvalido3
+
+caso5_6a = not $ receituarioValido receituarioInvalido4
+
+--------------------- EXERCÍCIO 5b ---------------------
+testeExercicio5a = and [caso5_1a, caso5_2a, caso5_3a, caso5_4a, caso5_5a, caso5_6a]
+
+caso5_1b = planoValido plano1
+
+caso5_2b = planoValido plano1
+
+caso5_3b = not $ planoValido planoInvalido1
+
+caso5_4b = not $ planoValido planoInvalido2
+
+caso5_5b = not $ planoValido planoInvalido3
+
+caso5_6b = not $ planoValido planoInvalido4
+
+testeExercicio5b = and [caso5_1b, caso5_2b, caso5_3b, caso5_4b, caso5_5b, caso5_6b]
+
+--------------------- EXERCÍCIO 6 ---------------------
+caso6_1 = plantaoValido plantao1
+
+caso6_2 = plantaoValido plantao2
+
+caso6_3 = plantaoValido plantao3
+
+caso6_4 = not (plantaoValido plantaoInvalido1)
+
+caso6_5 = not (plantaoValido plantaoInvalido2)
+
+caso6_6 = not (plantaoValido plantaoInvalido3)
+
+caso6_7 = not (plantaoValido plantaoInvalido4)
+
+testeExercicio6 = and [caso6_1, caso6_2, caso6_3, caso6_4, caso6_5, caso6_6, caso6_7]
+
+--------------------- EXERCÍCIO 7 ---------------------
+caso7_1 = geraPlanoReceituario receituario1 == [(6, [med6]), (8, [med4]), (17, [med4]), (22, [med7])]
+
+caso7_2 = geraPlanoReceituario receituario2 == [(6, [med6]), (8, [med4, med8]), (17, [med4]), (22, [med7, med8]), (23, [med8])]
+
+testeExercicio7 = and [caso7_1, caso7_2]
+
+--------------------- EXERCÍCIO 8 ---------------------
+caso8_1 = geraReceituarioPlano (geraPlanoReceituario receituario1) == receituario1
+
+caso8_2 = geraReceituarioPlano (geraPlanoReceituario receituario2) == receituario2
+
+testeExercicio8 = and [caso8_1, caso8_2]
+
+--------------------- EXERCÍCIO 9 ---------------------
+caso9_1 = isNothing (executaPlantao plantao1 estoque1)
+
+caso9_2 = executaPlantao plantao1 estoque2 == Just [(med4, 8), (med6, 4), (med7, 9)]
+
+caso9_3 = executaPlantao plantao2 estoque1 == Just [(med4, 8), (med6, 4), (med7, 29)]
+
+testeExercicio9 = and [caso9_1, caso9_2, caso9_3]
+
+--------------------- EXERCÍCIO 10 ---------------------
+caso10_1 = not (satisfaz plantao1 plano1 estoque1)
+
+caso10_2 = satisfaz plantao1 plano1 estoque2
+
+caso10_3 = satisfaz plantao2 plano1 estoque1
+
+testeExercicio10 = and [caso10_1, caso10_2, caso10_3]
+
+--------------------- EXERCÍCIO 11 ---------------------
+caso11_1 = satisfaz plantao plano1 estoque1
+  where
+    plantao = plantaoCorreto plano1 estoque1
+
+caso11_2 = satisfaz plantao plano2 estoque2
+  where
+    plantao = plantaoCorreto plano2 estoque2
+
+testeExercicio11 = and [caso11_1, caso11_2]
+
+--------------------- VERIFICAÇÃO DOS TESTES ---------------------
 main = hspec $ do
-
-  describe "Suite de Testes do Trabalho 01" $ do
-
+  describe "Suite de Testes do Trabalho 2" $ do
     it "O resultado global dos testes do exercício 1 deve 'True'" $ do
-      testeExercicio1 `shouldBe` True
+       testeExercicio1 `shouldBe` True
 
     it "O resultado global dos testes do exercício 2 deve 'True'" $ do
       testeExercicio2 `shouldBe` True
@@ -99,15 +288,25 @@ main = hspec $ do
     it "O resultado global dos testes do exercício 4 deve 'True'" $ do
       testeExercicio4 `shouldBe` True
 
-    it "O resultado global dos testes do exercício 5 deve 'True'" $ do
-      testeExercicio5 `shouldBe` True
+    it "O resultado global dos testes do exercício 5.1 deve 'True'" $ do
+      testeExercicio5a `shouldBe` True
+    it "O resultado global dos testes do exercício 5.2 deve 'True'" $ do
+      testeExercicio5b `shouldBe` True
 
     it "O resultado global dos testes do exercício 6 deve 'True'" $ do
       testeExercicio6 `shouldBe` True
 
-    it "O resultado global dos testes do exercício 7a deve 'True'" $ do
-      testeExercicio7a `shouldBe` True
-    it "O resultado global dos testes do exercício 7b deve 'True'" $ do
-      testeExercicio7b `shouldBe` True
-    it "O resultado global dos testes do exercício 7c deve 'True'" $ do
-      testeExercicio7c `shouldBe` True
+    it "O resultado global dos testes do exercício 7 deve 'True'" $ do
+      testeExercicio7 `shouldBe` True
+
+    it "O resultado global dos testes do exercício 8 deve 'True'" $ do
+      testeExercicio8 `shouldBe` True
+
+    it "O resultado global dos testes do exercício 9 deve 'True'" $ do
+      testeExercicio9 `shouldBe` True
+
+    it "O resultado global dos testes do exercício 10 deve 'True'" $ do
+      testeExercicio10 `shouldBe` True
+
+    it "O resultado global dos testes do exercício 11 deve 'True'" $ do
+      testeExercicio11 `shouldBe` True
